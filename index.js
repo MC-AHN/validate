52c9371d5f322bc307e3b91f9aee2e71100cee35
Muhammad <mh5458837@gmail.com>
2026-02-03 10:51:42 +0700

    PART 3: Create a POST method with if else statment

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ app.post("/register", async (c) => {
     return c.json({ message: "Registration data received", data: body });
 })
 
+app.post("/register-basic", async (c) => {
+    const body = await c.req.json()
+    const name = body.name;
+    const email = body.email;
+
+    // Validation Manual
+    // 1. Cek Data
+    if (!name) {
+        return c.json({ error: "Name is Required" }, 400);
+    }
+
+    // 2. Cek Mail
+    if (!email) {
+        return c.json({ error: "Email is Required" }, 400);
+    }
+
+    // 
+    return c.json({ message: "Reqister Completed", data:body })
+})
+
 const port = 8001;
 console.log(`Starting validation service on port ${port}...`);
 
