e870356b6e7da8218e92c01a42c922b97fbc3bd6
Muhammad <mh5458837@gmail.com>
2026-02-03 10:46:52 +0700

    PART 2: Create a POST method and try it

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get("/", (c) => {
     return c.text("Validation Service is Running");
 })
 
+app.post("/register", async (c) => {
+    // Get JSON body
+    const body = await c.req.json()
+
+    // cek required fields
+    console.log("Data: ", body);
+
+    return c.json({ message: "Registration data received", data: body });
+})
+
 const port = 8001;
 console.log(`Starting validation service on port ${port}...`);
 
